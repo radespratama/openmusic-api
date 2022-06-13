5a636f775cbe8cb760b1e012644d9c0429ab6e8a
radespratama <puturades89@gmail.com>
2022-06-13 08:53:27 +0800

    Fix file cover

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -44,7 +44,7 @@ const routes = (handler) => [
     path: "/albums/covers/{param*}",
     handler: {
       directory: {
-        path: path.resolve(__dirname, "file/covers"),
+        path: path.resolve(__dirname, "file/cover"),
       },
     },
   },
